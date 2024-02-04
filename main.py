dbe4417b48781e07ef9b8005aeb4606fa8f3a6b4
Garry <github@garrynet.co.uk>
2024-02-04 23:27:59 +0000

    pain

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+#!venv/bin/python
+
 # Made with ❤️ by Garry
 # 02/02/2024
 
-# main script - menus live here, calculations and plotting done in separate scripts which will be imported
+class menu:
+    def ree():
+        pass
+
+class data:
+    def ree():
+        pass
+
+if __name__ == "__main__":
+    # from InquirerPy import inquirer, separator
+    import matplotlib.pyplot as plt
+    # import csv
+
+    x = [1,2,3,4,5]
+    y = [5,4,3,2,1]
+
+    plt.scatter(x, y)
+    plt.savefig("plots/scatter.png")
