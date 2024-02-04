9275ec18d1a8254e18e4e854b12b0eb3a06bd13b
Garry <github@garrynet.co.uk>
2024-02-04 23:11:10 +0000

    removed overcomplications

--- src/calculate.py
+++
@@ -1,4 +0,0 @@
-# Made with ❤️ by Garry
-# 02/02/2024
-
-# to be imported by another script - calculate data from arrays passed in when this script it called.
--- src/plot.py
+++
@@ -1,4 +0,0 @@
-# Made with ❤️ by Garry
-# 02/02/2024
-
-# to be imported by another script - functions: plot data from provided values, export image file of the plot into ../plots
